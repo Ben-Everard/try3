6d928f7012b3bfda75411d826fbc5a215a008d1f
Dexter Clark <dexter.clark@gmail.com>
2015-02-28 18:47:35 -0800

    add business class

--- src/app/add-business/add-business.js
+++ src/app/add-business/add-business.js
@@ -1,10 +1,23 @@
 (function() {
     'use strict';
 
-    angular.module('add.business', ['businessify.variable'])
+    angular.module('add.business', [
+        'businessify.variable',
+        'businessify.business'
+    ])
     .controller('AddBusinessCtrl', AddBusinessCtrl);
 
-    function AddBusinessCtrl($scope, variables) {
-        $scope.business = {};
+    function AddBusinessCtrl($scope, Business, variables) {
+        $scope.businesses = Business.all;
+
+        $scope.buisness = {};
+
+        $scope.addBusiness = function() {
+            Business.create($scope.business);
+        };
+
+        $scope.deleteBusiness = function(business) {
+            Business.delete(business);
+        };
     }
 })();

4e73909fb7d26e58c256014dad6c37493f122c7f
Dexter Clark <dexter.clark@gmail.com>
2015-02-28 16:44:42 -0800

    clean up dependencies and add firebase

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,26 @@
 'use strict';
 
-angular.module('businessify', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ngMaterial'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+angular.module('businessify', [
+  'ngAnimate',
+  'ngCookies',
+  'ngTouch',
+  'ngSanitize',
+  'ngResource',
+  'ngRoute',
+  'ngMaterial',
+  'firebase'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/business', {
+      templateUrl: 'app/business/buisness.html',
+      controller: 'BusinessCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});

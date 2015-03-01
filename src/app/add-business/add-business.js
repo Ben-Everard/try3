79cd4b3bcc0c2157350e46edf2178592c680e942
Dexter Clark <dexter.clark@gmail.com>
2015-03-01 12:56:35 -0800

    simple form added

--- src/app/add-business/add-business.js
+++ src/app/add-business/add-business.js
@@ -2,22 +2,51 @@
     'use strict';
 
     angular.module('add.business', [
-        'businessify.variable',
         'businessify.business'
     ])
     .controller('AddBusinessCtrl', AddBusinessCtrl);
 
-    function AddBusinessCtrl($scope, Business, variables) {
+    function AddBusinessCtrl($scope, Business) {
         $scope.businesses = Business.all;
+        $scope.business = {
+            address: {},
+            types: []
+        };
 
-        $scope.buisness = {};
+        $scope.capacities = [
+            "5 - 25",
+            "25 - 50",
+            "50 - 100",
+            "100 - 200",
+            "200+"
+        ];
+        $scope.businessTypes = [
+            'Activity',
+            'Food',
+            'Event Planner',
+            'Hotel/Lodging',
+            'Photos',
+            'Transportation',
+            'Venue'
+        ];
 
         $scope.addBusiness = function() {
             Business.create($scope.business);
         };
 
+        $scope.addType = function() {
+            $scope.business.types.push($scope.businessType);
+            _.pull($scope.businessTypes, $scope.businessType);
+            $scope.businessType = '';
+        };
+
         $scope.deleteBusiness = function(business) {
             Business.delete(business);
+        };
+
+        $scope.removeActivity = function(type) {
+            _.pull($scope.business.types, type);
+            $scope.businessTypes.push(type);
         };
     }
 })();
